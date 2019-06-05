a256bf694a9f6e02b2c76ed87722eb13d92d467d
anthonydevelops <anthonydevelops@gmail.com>
2019-06-05 16:00:29 -0700

    Updated styling and layout
    Signed-off-by: anthonydevelops <anthonydevelops@gmail.com>

--- src/App.js
+++ src/App.js
@@ -9,31 +9,29 @@ class App extends React.Component {
     this.state = {
       content: null
     };
+
   }
 
-  // Handles text generation for each author
+  // --------------------------------------------
+  // Handles the text generation for each author
+  // --------------------------------------------
   contentHandler(id) {
-    // ----------------------------------
-    // Create new node on intertexuality
-    // ----------------------------------
+
+    // Intertextuality Node
     const p = document.createElement("p")
     p.setAttribute("id", "intertextuality")
     let text = document.createTextNode(content[id].intertextuality)
     p.appendChild(text)
 
-    // let parent = document.getElementById("content")
     let parent = document.getElementById("content-text")
     if (document.getElementById("intertextuality")) {
       const current = document.getElementById("intertextuality")
       parent.replaceChild(p, current)
     } else {
-      // parent.replaceChild(p, current)
       parent.appendChild(p)
     }
 
-    // ----------------------------------
-    // Create new node on episodes
-    // ----------------------------------
+    // Episodes Node
     const h3 = document.createElement("h3")
     h3.setAttribute("id", "episodes")
     const episodes = `Aires on Season ${content[id].seasons}, Episode ${content[id].episodes}`
@@ -47,11 +45,8 @@ class App extends React.Component {
     } else {
       parent.appendChild(h3)
     }
-    // parent.replaceChild(h3, current)
 
-    // ----------------------------------
-    // Create new node on background
-    // ----------------------------------
+    // Background Node
     const link = document.createElement("a")
     link.setAttribute("id", "link")
     text = document.createTextNode("Isabel Allende - Aphrodite")
@@ -66,7 +61,6 @@ class App extends React.Component {
     } else {
       parent.appendChild(link)
     }
-    // parent.replaceChild(link, current)
   }
 
   render() {
@@ -77,29 +71,101 @@ class App extends React.Component {
             <h1 className="title">Jane the Virgin</h1>
             <div className="sidebar">
               <ul>
-                <li><Button outline color="info" block onClick={(e) => this.contentHandler("gruesz", e)}>Kirsten Gruesz</Button></li>
-                <li><Button outline color="info" block onClick={(e) => this.contentHandler("kanellos", e)}>Nicolas Kanellos</Button></li>
-                <li><Button outline color="info" block onClick={(e) => this.contentHandler("allende", e)}>Isabel Allende</Button></li>
-                <li><Button outline color="info" block onClick={(e) => this.contentHandler("marquez", e)}>Gabriel Garcia Marquez</Button></li>
-                <li><Button outline color="info" block onClick={(e) => this.contentHandler("martin", e)}>Gerald Martin</Button></li>
-                <li><Button outline color="info" block onClick={(e) => this.contentHandler("borges", e)}>Jorge Luis Borges</Button></li>
-                <li><Button outline color="info" block onClick={(e) => this.contentHandler("martin-rodriguez", e)}>Manuel Martin-Rodriguez</Button></li>
-                <li><Button outline color="info" block onClick={(e) => this.contentHandler("lugo-lugo", e)}>Carmen Lugo-Lugo</Button></li>
-                <li><Button outline color="info" block onClick={(e) => this.contentHandler("castro-klaren", e)}>Sara Castro-Klaren</Button></li>
-                <li><Button outline color="info" block onClick={(e) => this.contentHandler("lichter-amundson", e)}>Robert Lichter & Daniel Amundson</Button></li>
-                <li><Button outline color="info" block onClick={(e) => this.contentHandler("nussbaum", e)}>Emily Nussbaum</Button></li>
-                <li><Button outline color="info" block onClick={(e) => this.contentHandler("itzkoff", e)}>Dave Itzkoff</Button></li>
-                <li><Button outline color="info" block onClick={(e) => this.contentHandler("johnson", e)}>Carroll Johnson</Button></li>
-                <li><Button outline color="info" block onClick={(e) => this.contentHandler("waugh", e)}>Patricia Waugh</Button></li>
-                <li><Button outline color="info" block onClick={(e) => this.contentHandler("martin-barbero", e)}>Jesus Martin-Barbero</Button></li>
-                <li><Button outline color="info" block onClick={(e) => this.contentHandler("rincon", e)}>Omar Rincon</Button></li>
-                <li><Button outline color="info" block onClick={(e) => this.contentHandler("pinon", e)}>Juan Pinon</Button></li>
-                <li><Button outline color="info" block onClick={(e) => this.contentHandler("erlick", e)}>June Carolyn Erlick</Button></li>
+                <li>
+                  <Button outline style={{ color: "black", backgroundColor: "rgb(213, 39, 183)" }} block onClick={(e) => this.contentHandler("gruesz", e)}>
+                    Kirsten Gruesz
+                  </Button>
+                </li>
+                <li>
+                  <Button outline style={{ color: "black", backgroundColor: "rgb(213, 39, 183)" }} block onClick={(e) => this.contentHandler("kanellos", e)}>
+                    Nicolas Kanellos
+                    </Button>
+                </li>
+                <li>
+                  <Button outline style={{ color: "black", backgroundColor: "rgb(213, 39, 183)" }} block onClick={(e) => this.contentHandler("allende", e)}>
+                    Isabel Allende
+                  </Button>
+                </li>
+                <li>
+                  <Button outline style={{ color: "black", backgroundColor: "rgb(213, 39, 183)" }} block onClick={(e) => this.contentHandler("marquez", e)}>
+                    Gabriel Garcia Marquez
+                  </Button>
+                </li>
+                <li>
+                  <Button outline style={{ color: "black", backgroundColor: "rgb(213, 39, 183)" }} block onClick={(e) => this.contentHandler("martin", e)}>
+                    Gerald Martin
+                  </Button>
+                </li>
+                <li>
+                  <Button outline style={{ color: "black", backgroundColor: "rgb(213, 39, 183)" }} block onClick={(e) => this.contentHandler("borges", e)}>
+                    Jorge Luis Borges
+                  </Button>
+                </li>
+                <li>
+                  <Button outline style={{ color: "black", backgroundColor: "rgb(213, 39, 183)" }} block onClick={(e) => this.contentHandler("martin-rodriguez", e)}>
+                    Manuel Martin-Rodriguez
+                  </Button>
+                </li>
+                <li>
+                  <Button outline style={{ color: "black", backgroundColor: "rgb(213, 39, 183)" }} block onClick={(e) => this.contentHandler("lugo-lugo", e)}>
+                    Carmen Lugo-Lugo
+                  </Button>
+                </li>
+                <li>
+                  <Button outline style={{ color: "black", backgroundColor: "rgb(213, 39, 183)" }} block onClick={(e) => this.contentHandler("castro-klaren", e)}>
+                    Sara Castro-Klaren
+                  </Button>
+                </li>
+                <li>
+                  <Button outline style={{ color: "black", backgroundColor: "rgb(213, 39, 183)" }} block onClick={(e) => this.contentHandler("lichter-amundson", e)}>
+                    Robert Lichter & Daniel Amundson
+                  </Button>
+                </li>
+                <li>
+                  <Button outline style={{ color: "black", backgroundColor: "rgb(213, 39, 183)" }} block onClick={(e) => this.contentHandler("nussbaum", e)}>
+                    Emily Nussbaum
+                  </Button>
+                </li>
+                <li>
+                  <Button outline style={{ color: "black", backgroundColor: "rgb(213, 39, 183)" }} block onClick={(e) => this.contentHandler("itzkoff", e)}>
+                    Dave Itzkoff
+                  </Button>
+                </li>
+                <li>
+                  <Button outline style={{ color: "black", backgroundColor: "rgb(213, 39, 183)" }} block onClick={(e) => this.contentHandler("johnson", e)}>
+                    Carroll Johnson
+                  </Button>
+                </li>
+                <li>
+                  <Button outline style={{ color: "black", backgroundColor: "rgb(213, 39, 183)" }} block onClick={(e) => this.contentHandler("waugh", e)}>
+                    Patricia Waugh
+                  </Button>
+                </li>
+                <li>
+                  <Button outline style={{ color: "black", backgroundColor: "rgb(213, 39, 183)" }} block onClick={(e) => this.contentHandler("martin-barbero", e)}>
+                    Jesus Martin-Barbero
+                  </Button>
+                </li>
+                <li>
+                  <Button outline style={{ color: "black", backgroundColor: "rgb(213, 39, 183)" }} block onClick={(e) => this.contentHandler("rincon", e)}>
+                    Omar Rincon
+                  </Button>
+                </li>
+                <li>
+                  <Button outline style={{ color: "black", backgroundColor: "rgb(213, 39, 183)" }} block onClick={(e) => this.contentHandler("pinon", e)}>
+                    Juan Pinon
+                  </Button>
+                </li>
+                <li>
+                  <Button outline style={{ color: "black", backgroundColor: "rgb(213, 39, 183)" }} block onClick={(e) => this.contentHandler("erlick", e)}>
+                    June Carolyn Erlick
+                  </Button>
+                </li>
               </ul>
             </div>
             <div id="content">
-              <h3 id="content-episodes"></h3>
-              <h3 id="content-background-link"></h3>
+              <div id="content-episodes"></div>
+              <div id="content-background-link"></div>
               <div id="content-text"></div>
             </div>
           </div>
